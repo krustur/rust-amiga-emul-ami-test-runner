0870a82070fcdd9c47074880658b66af968735ad
Krister Jansson <krustur@gmail.com>
2023-04-29 00:52:42 +0200

    Added test to test suite

--- test_suite.s
+++ test_suite.s
@@ -102,4 +102,53 @@ LSL_B_register_by_immediate_FAIL
 .assert_code
   LSL.B #6,D1
 
-;arne	equ *-86
+;======================================
+
+ADDI_W_immediate_data_to_absolute_long
+
+ dc.l .name		; $00
+ dc.l .arrange_mem	; $04
+ dc.l .arrange_regs	; $08
+ dc.l .arrange_code	; $0c
+ dc.l .assert_mem	; $10
+ dc.l .assert_regs	; $14
+ dc.l .assert_code	; $18
+
+.name
+ dc.b "ADDI_W_immediate_data_to_absolute_long",0
+ even
+
+.arrange_mem
+ ; length,address,ptr
+ dc.l $00000002,$00040000,.arrange_mem_00040000
+ dc.l $00000000
+
+.arrange_mem_00040000
+ dc.b $3c,$09
+
+.arrange_regs
+ ;    D0/A0     D1/A1     D2/A2     D3/A3     D4/A4     D5/A5     D6/A6     D7/A7
+ dc.l $00000000,$00000000,$00000000,$00000000,$00000000,$00000000,$00000000,$00000000
+ dc.l $00000000,$00000000,$00000000,$00000000,$00000000,$00000000,$00000000,$00000000
+ dc.w $0000 ; SR -----
+
+.arrange_code
+ ; length,address
+ dc.l $00000004,$00070000
+ dc.b $06,$79,$38,$78,$00,$04,$00,$00
+
+.assert_mem
+ dc.l $00000002,$00040000,.assert_mem_00040000
+ dc.l $00000000
+
+.assert_mem_00040000
+ dc.b $74,$81
+
+.assert_regs
+ ;    D0/A0     D1/A1     D2/A2     D3/A3     D4/A4     D5/A5     D6/A6     D7/A7
+ dc.l $00000000,$00000000,$00000000,$00000000,$00000000,$00000000,$00000000,$00000000
+ dc.l $00000000,$00000000,$00000000,$00000000,$00000000,$00000000,$00000000,$00000000
+ dc.w $0000 ; SR -----
+
+.assert_code
+ ADDI.W #$3878,($00040000).L
